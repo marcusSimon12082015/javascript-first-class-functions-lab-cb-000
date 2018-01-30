012bd51a9b4233ec840d6aa515eb3b6151d85388
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2018-01-30 19:58:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ function countdown(callback){
   window.setTimeout(callback,2000);
 }
 function createMultiplier(multiplierValue){
-   function f(n){ return n * multiplierValue;}
+   return function f(n){ return n * multiplierValue;}
 }
