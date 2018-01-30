4f51915240e92df2c72b373ff787c56753273073
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2018-01-30 20:16:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function createMultiplier(multiplierValue){
    return function f(n){ return n * multiplierValue;}
 }
 function multiplier(multiplierValue,value){
-  return function(){multiplierValue * value;}
+  return multiplierValue * value;
 }
 
 var doubler = multiplier.bind(null,2);
