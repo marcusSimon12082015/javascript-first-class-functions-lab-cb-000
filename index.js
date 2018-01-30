cb826cea6b7eba86d71e5500fc7d1e6dbe7d0b9b
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2018-01-30 20:34:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ function multiplier(multiplierValue,value){
   return multiplierValue * value;
 }
 
-var doubler = multipler.bind(null,2);
-var tripler = multipler.bind(null,3);
+var doubler = multiplier.bind(null,2);
+var tripler = multiplier.bind(null,3);
 
 var doublerWithBind = doubler;
 var triplerWithBind = tripler;
